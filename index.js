2cdf431489d762ba6cc5ee159f2824961ed78515
Alamito <alamirbobroski12@gmail.com>
2022-11-10 20:32:47 -0300

    feat - add store infos in the object

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');   // biblioteca de FileSystem do NodeJS
+const cheerio = require('cheerio');
+const { info } = require('console');
 
 const URL = "https://jc.ne10.uol.com.br/blogs/torcedor/2022/11/15117681-tabela-da-copa-do-mundo-2022-completa-veja-quando-comeca-quando-termina-e-todos-os-jogos-da-copa-do-mundo-do-catar.html"
 
@@ -39,7 +41,7 @@ const getPage = () => {
 const getCachedPage = () => {
     const fileName = "cache/World Cup 2022.html";
 
-    const promiseCallback = async (resolve, reject) => {
+    const promiseCallback = async (resolve) => {
 
         const cachedHTML = await readFromFile(fileName);
 
@@ -55,6 +57,47 @@ const getCachedPage = () => {
     return new Promise(promiseCallback);
 };
 
+const getPageItems = (html) => {
+
+    const $ = cheerio.load(html);
+
+    const promiseCallback = (resolve, reject) => {
+        const selectorTitle = '#textContentCount > h3:nth-child(n+9)';
+        const selectorGames = '#textContentCount > ul:nth-child(n+10) > li:nth-child(n+1)'
+        const info = {
+            Titles: [],
+            Day: [],
+            Games: [],
+        }
+
+        info.Titles = getPageText($, '#textContentCount > h3:nth-child(n+9)');
+        info.Day = getPageText($, '#textContentCount > h4:nth-child(n+17)');
+        info.Games = getPageText($, '#textContentCount > ul');
+
+        // writeToFile(info.Title[0], 'SaveFiles/teste.txt');
+
+        console.log(info.Day);
+        
+        resolve(true);
+    }
+
+    return new Promise(promiseCallback);
+}
+
+const getPageText = ($, selector) => {
+    const arr = [];
+    $(selector).each(function (i, element) { 
+        const a = $(element);
+        const text = a.text();
+
+        arr[i] = text;
+    })
+
+    return arr;
+}
+
+
 /* CASO DESSE ERROR, A SOLUCAO PODERIA SER COLOCAR OS HEADERS*/
 getCachedPage()
+    .then(getPageItems)
     .catch(console.error)
